245f84b8e9f59be04ac1870935c3b85fc5898710
JaimeDVP562 <jgavtor562@g.educaand.es>
2025-09-22 17:59:44 +0200

    ""

--- 01_Actividad_JaimeGavilanTorrero/www/index.php
+++ 01_Actividad_JaimeGavilanTorrero/www/index.php
@@ -1,5 +1,14 @@
-<?php phpinfo(
-); 
-echo "¡Jaime Gavilan Torrero alumno DAW2!";
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo ;
+    "<h1>Hola mundo</h1>"
+    ?>
+</body>
+</html>
