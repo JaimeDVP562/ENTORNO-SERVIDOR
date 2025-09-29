c2c9f0eed51c9e8be9915fccf8324fc15f381ae2
Jaime Gavilan Torrero <jgavtor562@g.educaand.es>
2025-09-29 19:30:23 +0200

    ""

---
+++ www/unidad2/act3/act3.1.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<?php
+/*
+---
+title: Unidad 2:  Actividad_2.8
+desc:  Nave espacial
+tags: [PHP, basico]
+---
+*/
+?>
+
+<body>
+    <?php
+    // Declaramos las variables
+    $numero1 = rand();
+    $numero2 = rand();
+    $numero3 = rand();
+
+    // Para mostrar el mensaje tendremos que utilizar un if-else
+    if (($numero1 > ($numero2 && $numero3))) {
+        echo ("El numero mayor es $numero1");
+    } elseif (($numero2 > ($numero1 && $numero3))) {
+        echo ("El numero mayor es $numero2");
+    } elseif (($numero3 > ($numero1 && $numero2))) {
+        echo ("El numero mayor es $numero3");
+    }
+    ?>
+
+    <ul>Juego: Mayor de tres números</ul>
+    <li><?php echo ("$numero1") ;?></li>
+
+
+</body><script type="module" src=""></script>
+
+</html>
