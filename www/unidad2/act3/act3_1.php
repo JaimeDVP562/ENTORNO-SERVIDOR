5cff4ab36de26b27e6a1cdeefb9e6fa725e61ed3
Jaime Gavilan Torrero <jgavtor562@g.educaand.es>
2025-10-01 19:31:59 +0200

    act3_2

--- www/unidad2/act3/act3_1.php
+++ www/unidad2/act3/act3_1.php
@@ -16,33 +16,38 @@ tags: [PHP, basico]
 */
 ?>
 <style>
-    div{
+    div {
         border: solid;
         text-align: center;
     }
 </style>
+
 <body>
     <?php
     // Declaramos las variables
     $numero1 = rand(1, 100);
     $numero2 = rand(1, 100);
     $numero3 = rand(1, 100);
+    $resultado = "";
 
     // Para mostrar el mensaje tendremos que utilizar un if-else
-    if (($numero1 > ($numero2 && $numero3))) {
-        echo ("El numero mayor es $numero1");
-    } elseif (($numero2 > ($numero1 && $numero3))) {
-        echo ("El numero mayor es $numero2");
-    } elseif (($numero3 > ($numero1 && $numero2))) {
-        echo ("El numero mayor es $numero3");
+    if ($numero1 > $numero2 && $numero1 > $numero3) {
+        $resultado = "El número mayor es $numero1";
+    } elseif ($numero2 > $numero1 && $numero2 > $numero3) {
+        $resultado = "El número mayor es $numero2";
+    } elseif ($numero3 > $numero1 && $numero3 > $numero2) {
+        $resultado = "El número mayor es $numero3";
+    } else {
+        $resultado = "Hay un empate entre los números mayores";
     }
+
     ?>
     <div>
         <ul>Juego: Mayor de tres números</ul>
-        <ul>Número 1 :<?php echo ($numero1) ?></ul>
-        <ul>Número 2 :<?php echo ($numero2) ?></ul>
+        <ul>Número 1 : <?php echo ($numero1) ?></ul>
+        <ul>Número 2 : <?php echo ($numero2) ?></ul>
         <ul>Número 3: <?php echo ($numero3) ?></ul>
-        <ul>El número mayor es: <?php echo ("$numero1") ?></ul>
+        <ul>El número mayor es: <?php echo ($resultado); ?></ul>
     </div>
 
 
---
+++ www/unidad2/act3/act3_3.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<?php
+/*
+---
+title: Unidad 2:  Actividad_2.8
+desc:  Nave espacial
+tags: [PHP, basico]
+---
+*/
+?>
+<style>
+    div {
+        border: solid;
+        text-align: center;
+    }
+</style>
+
+<body>
+    <?php
+    // Declaramos las variables
+    $nota = rand(1, 10);
+
+    $resultado = "";
+
+    // Para mostrar el mensaje tendremos que utilizar un if-else
+    if ($nota >= 0 && $nota < 5) {
+        $resultado = "Insuficiente";
+    } elseif ($nota >= 5 && $nota <= 6) {
+        $resultado = "Suficiente";
+    } elseif ($nota >= 7 && $nota <= 8){
+        $resultado = "Notable";
+    } else {
+        $resultado = "Sobresaliente";
+    }
+
+    ?>
+    <div>
+        <ul>Simulación de Calificación.</ul>
+        <ul>La nota generada es : <?php echo ($nota) ?></ul>
+        <ul>Calificación : <?php echo ($resultado) ?></ul>
+    </div>
+
+
+</body>
+
+</html>
