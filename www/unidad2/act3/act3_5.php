e23f3968477239eacf2b9bb772f1537d047c0c21
Jaime Gavilan Torrero <jgavtor562@g.educaand.es>
2025-10-02 16:59:43 +0200

    ""

---
+++ www/unidad2/act3/act3_5.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<?php
+/*
+---
+title: Unidad 2:  Actividad_3_4
+desc:  Mostramos numeros pares dentro de un rango de numeros
+tags: [PHP, basico]
+---
+*/
+?>
+<style>
+    div {
+        border: solid;
+        text-align: center;
+    }
+</style>
+
+<body>
+    <div>
+        <p>Números pares del 0 al 50</p>
+
+        <?php
+        // Declaramos las variables 
+        $numero = 0;
+        $mensaje = 0;
+
+        while ($numero <= 50) {
+            if ($numero % 2 == 0) {
+                echo "<ul>$numero</ul>";
+            }
+            $numero++;
+        }
+
+        ?>
+    </div>
+
+
+
+</body>
+
+</html>
