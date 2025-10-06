f6345cb7a79445f5aff6e71b48a1e4bdfcdca3da
Jaime Gavilan Torrero <jgavtor562@g.educaand.es>
2025-10-06 16:43:49 +0200

    act3_7

---
+++ www/unidad2/act3/act3_7/act3_7.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<?php
+/*
+---
+title: Unidad 2:  Actividad_3_7
+desc:  Pareja de dados o no
+tags: [PHP, basico]
+---
+*/
+?>
+<style>
+    table {
+
+        width: 50%;
+        margin: 20px auto;
+    }
+
+    th,
+    td {
+        border: 2px solid #333;
+        padding: 10px;
+        text-align: left;
+    }
+
+    thead {
+        background-color: #f0f0f0;
+    }
+</style>
+
+<body>
+    <p>Ejemplos: </p>
+
+
+    <div>
+        <?php
+        // Declaramos las variables 
+        $dado1 = rand(1, 6);
+        $dado2 = rand(1, 6);
+
+        $respuesta = "";
+
+        // Utilizamos if-else para mostrar comprobar los resultados
+        if ($dado1 == $dado2) {
+            echo ("<p>Ha sacado una pareja de $dado1.</p>");
+        } else {
+            echo ("<p>No has sacado pareja. </p> ");
+        }
+
+        ?>
+        <img src="img/<?php echo ($dado1); ?>.svg" alt="">
+        <img src="img/<?php echo ($dado2); ?>.svg" alt="">
+    </div>
+
+
+
+</body>
+
+</html>
