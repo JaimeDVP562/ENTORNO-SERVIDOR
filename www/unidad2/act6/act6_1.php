13fb19ed3a9ea146788a158563b20995ab28a236
Jaime Gavilan Torrero <jgavtor562@g.educaand.es>
2025-10-11 19:38:51 +0200

    act6_1

---
+++ www/unidad2/act6/act6_1.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    // Declaramos las variables
+    $texto = "Este es mi comentario para soporte técnico.";
+    $limiteCaracteres = 160;
+  
+
+    // Obtenemos números de caracteres
+    $cantidadCaracteres = strlen($texto);
+    $resultado = ($limiteCaracteres - $cantidadCaracteres);
+    
+    // Mostramos por pantalla 
+    echo "<h2>Comentario</br></h2>";
+    echo "Este es mi comentario para soporte técnico.</br></br>";
+    echo "Longitud:  <strong>$cantidadCaracteres</strong>  caracteres</br></br>";
+    echo "Te quedan $resultado caracteres";
+
+    // Calculamos cuantos caracteres faltan para los caracteres establecidos
+    ?>
+</body>
+
+</html>
