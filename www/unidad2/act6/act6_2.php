06ff53f13d13ab6f7a1c751342189520c5103562
Jaime Gavilan Torrero <jgavtor562@g.educaand.es>
2025-10-11 19:55:33 +0200

    ""

---
+++ www/unidad2/act6/act6_2.php
@@ -0,0 +1,25 @@
+<?php
+/*
+---
+title: Unidad 2:  Actividad_6_1
+desc:  Array --> Cuenta caracteres de un texto
+tags: [PHP, basico]
+---
+*/
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    
+    ?>
+</body>
+
+</html>
