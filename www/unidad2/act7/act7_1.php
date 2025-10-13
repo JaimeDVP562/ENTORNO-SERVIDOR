8aaa09d69bcbf3be73cf830540fa212f2e20b80e
Jaime Gavilan Torrero <jgavtor562@g.educaand.es>
2025-10-13 12:22:21 +0200

    act7_1

---
+++ www/unidad2/act7/act7_1.php
@@ -0,0 +1,33 @@
+<?php
+/*
+---
+title: Unidad 2:  Actividad_7_1
+desc:  Array --> FUNCIONES: Numero impar 
+tags: [PHP, basico]
+---
+*/
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    // Declaramos las variables 
+    $numeroRamdon = rand(1,10);
+    
+    // Creamos la funcion 
+    function parImpar(int $num) : string{
+        $resultado = ($num % 2 === 0) ? "El numero $num es par" : "El numero $num es impar";
+
+        return $resultado;
+    }
+
+    // Llamamos a la funcion
+    echo parImpar($numeroRamdon);
+    ?>
+</body>
+</html>
