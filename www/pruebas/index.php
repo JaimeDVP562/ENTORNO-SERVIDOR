808fdc57fa708cd00dfb229bae7a148c106401bf
Jaime Gavilan Torrero <jgavtor562@g.educaand.es>
2025-11-07 18:13:23 +0100

    ""

---
+++ www/pruebas/index.php
@@ -0,0 +1,15 @@
+<?php
+
+    $dsn = 'mysql:dbname=dwes;host=db';
+    $usuario = 'root';
+    $contraseña = 'root';
+
+    try {
+        $mbd = new PDO($dsn, $usuario, $contraseña);
+        $mbd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo 'Conexión exitosa a la base de datos.';
+
+    } catch (PDOException $e) {
+        echo 'Falló la conexión: ' . $e->getMessage();
+    }
+?>
