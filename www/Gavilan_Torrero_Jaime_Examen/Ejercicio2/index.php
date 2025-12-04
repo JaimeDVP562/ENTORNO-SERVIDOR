53c1b9b9167adc7d6a85a73a41a300378c8cbbb2
Jaime Gavilan Torrero <jgavtor562@g.educaand.es>
2025-12-04 19:32:52 +0100

    ""

--- www/Gavilan_Torrero_Jaime_Examen/Ejercicio2/index.php
+++ www/Gavilan_Torrero_Jaime_Examen/Ejercicio2/index.php
@@ -1,44 +1,49 @@
 <?php
+// Iniciamos sesion siempre 
 session_start();
-$numeroSecreto = 2;
-$contador = 0;
+// Creamos las variables de sesion
+if(!isset($_SESSION['numeroSecreto'])){
+    $_SESSION['numeroSecreto'] = 7;
+}
+if (!isset($_SESSION['contadorIntentos'])) {
+    $_SESSION['contadorIntentos'] = -1 ;
+}
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    if (empty($_POST['intentoUsuario'])) {
-        echo "Numero incorrecto";
-        $contador++;
-    }elseif ($_POST['intentoUsuario'] > $numeroSecreto){
-        echo "El número es mayor";
-        $contador++;
-    }elseif ($_POST['intentoUsuario'] < $numeroSecreto){
-        echo "El número es menor";
-        $contador++;
-    }else{
-        echo "¡¡¡Acertaste ¡¡¡ Has necesitado $contador aciertos";
-        header("Location: ./cerrar-sesion.php");
+// Registramos con una variable de seccion el post del usuario
+if(isset($_POST['numeroUsuario'])){
+    $_SESSION['numeroUsuario'] = $_POST['numeroUsuario'];
+    $_SESSION['contadorIntentos']++;
+}
+// Comprobamos la logica para ver si el usuario acierta o no
+if (empty($_POST)) {
+    $mensaje ="";
+} else {
+    if ($_POST['numeroUsuario'] > $_SESSION['numeroSecreto']) {
+        $mensaje = "El número secreto es menor";
+    }elseif ($_POST['numeroUsuario'] < $_SESSION['numeroSecreto']) {
+        $mensaje= "El número secreto es mayor";
+    }elseif($_POST['numeroUsuario'] == $_SESSION['numeroSecreto']){
+        $mensaje = "¡Has acertado! Has necesitado " . $_SESSION['contadorIntentos'] . " intentos.";
+        unset($_SESSION);
+        session_destroy();
     }
 }
 ?>
-<?php
-?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./pico.min.css">
     <title>Document</title>
 </head>
-
 <body>
     <main class="container">
-        <form method="post" action=""></form>
-        <h2>Número Secreto: Elige número entre 1 y 10</h2>
-        <P>Número</P>
-        <label for=""><input type="text"></label>
-        <button name="intentoUsuario">Enviar</button>
+        <form action="" method="post">
+            <h2>Introduzca un número</h2>
+            <label for="numeroUsuario"><input type="number" name="numeroUsuario"></label>
+            <button type = "submit">Enviar</button>
+        </form>
+        <p><?php echo $mensaje;?></p>
     </main>
 </body>
-
 </html>
